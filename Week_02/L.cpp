fae503b96e3911c09d7deba7ed1a1d27a0525878
Arpita Das <arpita1477@gmail.com>
2026-04-18 14:32:13 +0600

    Add files via upload

---
+++ Week_02/L.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+ 
+int main(){
+    long long n;
+    cin>>n;
+    long long a[n+1];
+    long long moves=0;
+    for(int i=0;i<n;i++){
+        cin>>a[i];
+    }
+    for(long long i=1;i<n;i++){
+        if(a[i]<a[i-1]){
+            moves+=a[i-1]-a[i];
+            a[i]=a[i-1];
+        }
+    }
+    cout<<moves<<endl;
+}
